c45a31847073c8ac08a04f9f291cd61449911465
tovfikur <tovfikurrahman@gmail.com>
2025-11-08 17:17:53 +0600

    Update module for Odoo App Store: Add OPL-1 license, update metadata, professional README

--- developer_terminal/controllers/terminal_controller.py
+++ developer_terminal/controllers/terminal_controller.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# License OPL-1.0 or later (https://www.odoo.com/documentation/16.0/legal/licenses.html)
+# Copyright (c) 2025 Tovfikur Rahman (kendroo.io)
 
 import json
 import subprocess
